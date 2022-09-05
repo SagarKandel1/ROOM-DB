29249d3bcb8f53d6c3628a941cf300b4f513bc9d
SagarKandel1 <93831966+SagarKandel1@users.noreply.github.com>
2022-09-05 18:25:14 +0100

    Add files via upload

--- src/main/java/com/example/formtask/Model/UserRepo.kt
+++ src/main/java/com/example/formtask/Model/UserRepo.kt
@@ -1,13 +1,7 @@
 package com.example.formtask.Model
 
-import androidx.lifecycle.LiveData
-
 class UserRepo (private val userDao: UserDao) {
-    val readAllData: LiveData<List<User>> = userDao.readAllData()
 
-        suspend fun addUser(user: User){
-            userDao.addUser(user)
-    }
 }
 
 
